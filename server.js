2923f2b1f9b549306a6f5236ab2a16757f2b21fb
cduwors <christinaduwors@gmail.com>
2022-10-03 10:30:00 -0400

    ready to test user routes in Insomnia

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(
+	// process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-media-api',
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-media-api",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+// Use this to log mongo queries being executed
+mongoose.set("debug", true);
+
+app.use(require("./routes"));
+mongoose.connection.once("open", () => {
+	app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+});
